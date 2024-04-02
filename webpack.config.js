3c9bcde250acb51dc69f49d9133255ba03df72bd
Triangular100 <139834502+Triangular100@users.noreply.github.com>
2024-04-02 10:47:50 -0600

    Remove dev mode

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,9 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: 'development',
+    // mode: 'development',
     entry: './src/index.js',
-    devtool: 'inline-source-map',
+    // devtool: 'inline-source-map',
     devServer: {
         static: './dist',
     },
