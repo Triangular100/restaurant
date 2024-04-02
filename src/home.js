942dc939f3908ccd260653b4770825b91fb11a86
Triangular100 <139834502+Triangular100@users.noreply.github.com>
2024-04-02 10:29:58 -0600

    Add home page

--- src/home.js
+++ src/home.js
@@ -1,3 +1,78 @@
+import FacebookIcon from "./img/facebook-icon.svg";
+
 export default function loadHome() {
-    console.log("Home");
+    const content = document.getElementById("content");
+    content.appendChild(createHome());
+}
+
+function createHome() {
+    const home = document.createElement("div");
+    home.id = "home";
+    home.appendChild(createContent());
+    home.appendChild(createImage());
+    return home;
+}
+
+function createContent() {
+    const homeContent = document.createElement("div");
+    homeContent.classList.add("home-content");
+    homeContent.appendChild(createDescription());
+    homeContent.appendChild(createTitle());
+    homeContent.appendChild(createMenuButton());
+    homeContent.appendChild(createContact());
+    return homeContent;
+}
+
+function createDescription() {
+    const desc = document.createElement("p");
+    desc.classList.add("description");
+    desc.innerHTML = "Discover your taste";
+    return desc;
+}
+
+function createTitle() {
+    const title = document.createElement("p");
+    title.classList.add("title");
+    title.innerHTML = "Delta Dining";
+    return title;
+}
+
+function createMenuButton() {
+    const button = document.createElement("button");
+    button.classList.add("menu-button");
+    button.innerHTML = "Discover Menu";
+    return button;
+}
+
+function createContact() {
+    const contact = document.createElement("div");
+    contact.classList.add("contact");
+    contact.appendChild(createLine());
+    contact.appendChild(createFacebookIcon());
+    return contact;
+}
+
+function createLine() {
+    const line = document.createElement("div");
+    line.classList.add("line");
+    return line;
+}
+
+function createFacebookIcon() {
+    const link = document.createElement("a");
+    link.href = "#";
+    link.target = "_blank";
+
+    const icon = new Image();
+    icon.src = FacebookIcon;
+    icon.classList.add("icon");
+
+    link.appendChild(icon);
+    return link;
+}
+
+function createImage() {
+    const image = document.createElement("div");
+    image.classList.add("image");
+    return image;
 }
--- src/index.js
+++ src/index.js
@@ -7,21 +7,36 @@ import "./style.css";
 function loadPage() {
     loadHeader();
     loadHome();
-    bindNavEvents();
+    bindEvents();
 }
 
-function bindNavEvents() {
+function bindEvents() {
     const content = document.getElementById("content");
     const homeButton = document.getElementById("home");
     const menuButton = document.getElementById("menu");
     const aboutButton = document.getElementById("about");
+    bindHomeEvents(content, homeButton, menuButton, aboutButton);
+    bindNavEvents(content, homeButton, menuButton, aboutButton);
+}
 
+function bindHomeEvents(content, homeButton, menuButton, aboutButton) {
+    document.querySelector(".menu-button").addEventListener("click", () => {
+        homeButton.classList.remove("active");
+        menuButton.classList.add("active");
+        aboutButton.classList.remove("active");
+        content.innerHTML = "";
+        loadMenu();
+    });
+}
+
+function bindNavEvents(content, homeButton, menuButton, aboutButton) {
     homeButton.addEventListener("click", () => {
         homeButton.classList.add("active");
         menuButton.classList.remove("active");
         aboutButton.classList.remove("active");
         content.innerHTML = "";
         loadHome();
+        bindHomeEvents(content, homeButton, menuButton, aboutButton);
     });
 
     menuButton.addEventListener("click", () => {
